40c82ebe9277e7f5d9704f2513538afce6a76363
Shantanu Raj <s@sraj.me>
2020-10-23 16:41:32 +0530

    style: Improve global styles

--- src/Characters/index.tsx
+++ src/Characters/index.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import { Character } from "../Character";
 import { RnMCharacter } from "../types";
 
+import './characters.css'
+
 export function Characters({ data }: { data: RnMCharacter[] }) {
   return (
     <ul>
